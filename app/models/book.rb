029b0c0e44f9125a3edcdc8f5a5939904342d57f
AnnaWijetunga <annawijetunga@gmail.com>
2020-02-07 10:44:19 -0500

    Create validations and associations for Book model

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,12 @@
 class Book < ApplicationRecord
+    validates :author_first, presence: true
+    validates :title, presence: true, uniqueness: { case_sensitive: false }
+
+    belongs_to :genre
+    has_many :reviews
+    has_many :users, through: :reviews
+
+    # accepts_nested_attributes_for :reviews -- I think I'll need this
+
+    # will eventually include scope here, maybe by title and author
 end
