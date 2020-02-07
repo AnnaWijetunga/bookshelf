4316c02762a7efddcd455a3e1f20e7ef8cff9a9b
AnnaWijetunga <annawijetunga@gmail.com>
2020-02-07 15:40:26 -0500

    Updated alternative route

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   resources :books
 
   get '/auth/:provider/callback' => 'sessions#omniauth'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # get 'auth/:provider/callback', to: 'sessions#create'
 end
