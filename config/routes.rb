ee482a789274e0b8b744a71139598a63ad541725
AnnaWijetunga <annawijetunga@gmail.com>
2020-02-07 15:27:29 -0500

    Add OmniAuth callback route for our Google login

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   resources :reviews
   resources :users
   resources :books
+
+  get '/auth/:provider/callback' => 'sessions#omniauth'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
