5384b3c5f4ab0e5d96da2c7df452b1df84b1e69b
AnnaWijetunga <annawijetunga@gmail.com>
2020-02-07 10:29:00 -0500

    Add validations, password macro and assocations to the User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ApplicationRecord
+    validates :name, presence: true
+    validates :email, presence: true 
+    # I want email to be present as well as unique - how to write
+
+    has_secure_password
+
+    has_many :reviews
+    has_many :books, through: :reviews
+
+    # eventually will need google authentication model here
+
 end
