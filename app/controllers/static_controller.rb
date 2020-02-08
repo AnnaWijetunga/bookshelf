bc908e809c77603f7212f834c50d63728d6db674
AnnaWijetunga <annawijetunga@gmail.com>
2020-02-08 05:20:47 -0500

    Create and update StaticController with home route

---
+++ app/controllers/static_controller.rb
@@ -0,0 +1,14 @@
+class StaticController < ApplicationController
+    def home
+    end
+
+# Possibly title and author methods will live here
+
+    # def title
+    #     @books = Book.order_by_title
+    # end
+
+    # def author
+    #     @books = Book.order_by_author
+    # end
+end
