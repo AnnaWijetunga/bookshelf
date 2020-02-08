3906f39c292d80e2f9acc6a15f90ed986138aa26
AnnaWijetunga <annawijetunga@gmail.com>
2020-02-08 13:32:26 -0500

    Fill out most of create and destroy methods - minus the messages

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,24 @@
 class SessionsController < ApplicationController
+
+    # many methods moved to sessions_helper.rb to keep this clean
+
     def new
     end
 
     def create
-        # complete this method
+        if params[:session]
+            user = User.find_by(email: params[:session][:email])
+            valid_regular_user(user)
+        else
+            user = User.create_by_google(auth)
+            valid_google_user(user)
+        end
     end
 
     def destroy
-        # complete this method
+        log_out
+        # goodbye message
+        redirect_to root_path
     end
 
     private
