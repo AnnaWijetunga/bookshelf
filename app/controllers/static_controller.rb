9f4cfc499df21ff2d26f96e0af1eb05407d62a94
AnnaWijetunga <annawijetunga@gmail.com>
2020-02-09 13:10:43 -0500

    Add title and author routes to show ordered lists

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -2,13 +2,11 @@ class StaticController < ApplicationController
     def home
     end
 
-# Possibly title and author methods will live here
+    def title
+        @books = Book.order_by_title
+    end
 
-    # def title
-    #     @books = Book.order_by_title
-    # end
-
-    # def author
-    #     @books = Book.order_by_author
-    # end
+    def author
+        @books = Book.order_by_author
+    end
 end
