5a069d0ca1a0014d5123001c22052ce773a1d7bf
AnnaWijetunga <annawijetunga@gmail.com>
2020-02-11 13:05:12 -0500

    Comment out seed data - rake db:reset to clear data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,46 +1,46 @@
-require 'faker'
-include Faker
+# require 'faker'
+# include Faker
 
-30.times do
-    User.create(
-        name: Faker::Name.name,
-        email: Faker::Internet.email,
-        password: Faker::Internet.password
-    )
-end
+# 30.times do
+#     User.create(
+#         name: Faker::Name.name,
+#         email: Faker::Internet.email,
+#         password: Faker::Internet.password
+#     )
+# end
 
-30.times do
-    Genre.create(
-        name: Faker::Book.genre
-    )
-end
+# 30.times do
+#     Genre.create(
+#         name: Faker::Book.genre
+#     )
+# end
 
-30.times do
-    Review.create(
-        content: Faker::Restaurant.review,
-        book_id: Faker::Number.within(range: 1..30),
-        user_id: Faker::Number.within(range: 1..30)
-    )
-end
+# 30.times do
+#     Review.create(
+#         content: Faker::Restaurant.review,
+#         book_id: Faker::Number.within(range: 1..30),
+#         user_id: Faker::Number.within(range: 1..30)
+#     )
+# end
 
-30.times do
-    Book.create(
-        title: Faker::Book.title,
-        author_first: Faker::Name.first_name,
-        author_last: Faker::Name.last_name,
-        genre_id: Faker::Number.within(range: 1..30)
-    )
-end
+# 30.times do
+#     Book.create(
+#         title: Faker::Book.title,
+#         author_first: Faker::Name.first_name,
+#         author_last: Faker::Name.last_name,
+#         genre_id: Faker::Number.within(range: 1..30)
+#     )
+# end
 
 
-# Pre faker gem:
+# # Pre faker gem:
 
-User.create(name: "Banana Gal", email: "banana@email.com", password: "goat", password_confirmation: "goat")
+# User.create(name: "Banana Gal", email: "banana@email.com", password: "goat", password_confirmation: "goat")
 
-Genre.create(name: "Non-Fiction")
+# Genre.create(name: "Non-Fiction")
 
-Book.create(title: "Dear Mother" , author_first: "Bunmi", author_last: "Laditan" , genre_id: 1)
-Book.create(title: "Moby Dick" , author_first: "Herman", author_last: "Melville" , genre_id: 2)
+# Book.create(title: "Dear Mother" , author_first: "Bunmi", author_last: "Laditan" , genre_id: 1)
+# Book.create(title: "Moby Dick" , author_first: "Herman", author_last: "Melville" , genre_id: 2)
 
-Review.create(content: "This is the review #1 in the seed file! This book will bring you to tears of joy and laughter." , book_id: 1, user_id: 1)
-Review.create(content: "If you have a year to spare, this is perfect novel for you." , book_id: 3, user_id: 2)
+# Review.create(content: "This is the review #1 in the seed file! This book will bring you to tears of joy and laughter." , book_id: 1, user_id: 1)
+# Review.create(content: "If you have a year to spare, this is perfect novel for you." , book_id: 3, user_id: 2)
