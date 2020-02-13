8125d4b486858e756cc192c19980bc976a802f07
AnnaWijetunga <annawijetunga@gmail.com>
2020-02-13 10:01:11 -0500

    Add uid to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_06_202328) do
+ActiveRecord::Schema.define(version: 2020_02_13_140849) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 2020_02_06_202328) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "uid"
   end
 
 end
