41714d81a283e83dfc7b7733259346895acc83c5
AnnaWijetunga <annawijetunga@gmail.com>
2020-06-15 15:48:28 -0400

    Add possible Heroku config to fix oauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
     provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"],ENV["GOOGLE_CLIENT_SECRET"], skip_jwt: true
+
+    # unsure about this but it came recommended:
+    
+    # heroku config:add PLUGIN_HEROKU_CLIENT_ID=<CLIENT_ID_FROM_HEROKU> PLUGIN_HEROKU_CLIENT_SECRET=<CLIENT_SECRET_FROM_HEROKU>
 end
