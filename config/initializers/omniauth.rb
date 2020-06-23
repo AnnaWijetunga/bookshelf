fbabb54ee8f4be54eea02479c29291b431281ea7
AnnaWijetunga <annawijetunga@gmail.com>
2020-06-23 15:33:15 -0400

    Add heroku confit:get to get OAuth working

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,10 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-    provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"],ENV["GOOGLE_CLIENT_SECRET"], skip_jwt: true
+    # original - pre-Heroku hosting
+    # provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"],ENV["GOOGLE_CLIENT_SECRET"], skip_jwt: true
 
+    # updated, because of Heroku
+    heroku config:get, ENV["GOOGLE_CLIENT_ID"],ENV["GOOGLE_CLIENT_SECRET"], skip_jwt: true
+   
     # unsure about this but it came recommended:
-    
     # heroku config:add PLUGIN_HEROKU_CLIENT_ID=<CLIENT_ID_FROM_HEROKU> PLUGIN_HEROKU_CLIENT_SECRET=<CLIENT_SECRET_FROM_HEROKU>
 end
