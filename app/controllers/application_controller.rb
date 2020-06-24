f543895c10ef1b6a53471339035b7b43da7ba82a
AnnaWijetunga <annawijetunga@gmail.com>
2020-06-23 20:42:31 -0400

    Add skip before filter to allow OAuth and Google login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
     include SessionsHelper
+
+    # experimenting with before filter to allow access to Heroku/OAuth
+    skip_before_filter :protect_from_forgery
+    
 end
